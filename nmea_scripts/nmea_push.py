1858abc2988f775a4b6021955ddafe68d056d19a
Zopa <velalab.asso@gmail.com>
2026-05-05 14:35:24 +0200

    Ajout logs NMEA + scripts

--- nmea_scripts/nmea_push.py
+++ nmea_scripts/nmea_push.py
@@ -26,6 +26,7 @@ def git_push():
     try:
         os.chdir(REPO_PATH)
 
+        # 1. voir si changements
         result = subprocess.run(
             ["git", "status", "--porcelain"],
             capture_output=True,
@@ -36,6 +37,7 @@ def git_push():
             print("Aucun changement à push")
             return
 
+        # 2. stage + commit d'abord
         subprocess.run(["git", "add", "."], check=True)
 
         subprocess.run(
@@ -43,13 +45,16 @@ def git_push():
             check=True
         )
 
+        # 3. sync avec GitHub
+        subprocess.run(["git", "pull", "--rebase"], check=True)
+
+        # 4. push
         subprocess.run(["git", "push"], check=True)
 
         print("Push GitHub OK")
 
     except Exception as e:
         print(f"Erreur Git: {e}")
-
 
 # =========================================================
 # MAIN
